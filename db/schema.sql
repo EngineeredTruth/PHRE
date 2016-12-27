6e50b3184028d0e41a88cd659daed2cc9611f9bb
Engineered Truth <Tangoforthemango@gmail.com>
2016-12-27 08:14:46 -0800

    setting up sql database

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE listings
+(
+  id serial primary key,
+  name varchar(100),
+  developer varchar(40)
+  project_type varchar(30),
+  listing_type varchar(30),
+  units varchar(10)[],
+  price integer,
+  address varchar(255),
+  location varchar(255)
+);
+
+CREATE TABLE pictures
+(
+  listings_name varchar(100) references listings(name),
+  index integer,
+  thumb boolean,
+  title varchar(100),
+);
+
+CREATE TABLE users
+(
+  id serial primary key,
+  name varchar(60),
+  external_id varchar(100)
+)
