66f3b3054a6a6b95b3259b109c428bfac440b029
Engineered Truth <Tangoforthemango@gmail.com>
2016-12-28 10:04:28 -0800

    test

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE listings
 (
   id serial primary key,
   name varchar(100),
-  developer varchar(40)
+  developer varchar(40),
   project_type varchar(30),
   listing_type varchar(30),
   units varchar(10)[],
@@ -13,10 +13,10 @@ CREATE TABLE listings
 
 CREATE TABLE pictures
 (
-  listings_name varchar(100) references listings(name),
+  listings_name varchar(100),
   index integer,
   thumb boolean,
-  title varchar(100),
+  title varchar(100)
 );
 
 CREATE TABLE users
@@ -24,4 +24,4 @@ CREATE TABLE users
   id serial primary key,
   name varchar(60),
   external_id varchar(100)
-)
+);
---
+++ server/server.js
@@ -0,0 +1,61 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import session from 'express-session';
+import passport from 'passport';
+import massive from 'massive';
+import config from './config.json'
+
+const Auth0Strategy = require('passport-auth0')
+
+const massiveInstance = massive.connectSync({
+  connectionString: config.connectionString
+})
+
+const app = module.exports = express();
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.json());
+app.use(cors());
+app.set('db', massiveInstance);
+
+app.use(express.static(__dirname + '/../public'));
+
+const db = app.get('db');
+// const ctrl = require('./ctrl.js');
+
+var strategy = new Auth0Strategy({
+        domain: 'tran.auth0.com',
+        clientID: config.auth0ClientId,
+        clientSecret: config.auth0Secret,
+        callbackURL: config.callbackURL
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+      console.log("What's DB: ", db)
+
+
+        return done(null, profile);
+    }
+);
+
+passport.use(strategy);
+
+app.get('/auth/', passport.authenticate('auth0'));
+app.get('/callback', passport.authenticate('auth0', {
+    failureRedirect: '/auth',
+    successRedirect: '/'
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user); // put the whole user object from YouTube on the sesssion;
+});
+
+passport.deserializeUser((obj, done) => {
+    //Only do something here that needs to be done with every request
+    done(null, obj); // get data from session and put it on req.user in every endpoint;
+});
+
+app.listen(config.port, function() {
+    console.log('Hosting port', config.port);
+});
